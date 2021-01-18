3f841cd4f6bc387d514772ce013553fc095f3da7
Anicet Togue <atogue@gmail.com>
2021-01-18 11:55:20 +0100

    init project

---
+++ src/main/java/org/craftchain/market/hystrix/MarketHystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package org.craftchain.market.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketHystrixDashboardApplication.class, args);
+	}
+
+}
